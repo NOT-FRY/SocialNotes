1d90a6eccdfeb6bf61897cc2144f9b2928f44042
Ackermann32 <a.califano123@gmail.com>
2021-06-11 17:40:38 +0200

    PRova

---
+++ src/Servlet.java
@@ -0,0 +1,13 @@
+
+public class Servlet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             System.out.println("Questa è la prima servlet");
+             System.out.println("Forza GIUVE SIUUUUMM");
+
+             System.out.println("Questa è la seconda servlet");
+
+	}
+
+}
