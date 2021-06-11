bdd8fe6d9b6aff8d1c62b5baa36b3d41c5fceb2a
Ackermann32 <a.califano123@gmail.com>
2021-06-11 17:12:55 +0200

    ALfonso2

--- src/Servlet.java
+++ src/Servlet.java
@@ -5,7 +5,7 @@ public class Servlet {
 		// TODO Auto-generated method stub
              System.out.println("Questa è la prima servlet");
              System.out.println("Forza GIUVE SIUUUUMM");
-
+             System.out.println("VIVA STALIN U.R.S.S.");
              System.out.println("Questa è la seconda servlet");
 
 	}
