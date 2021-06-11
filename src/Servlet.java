0a30c92f58a885f9ae40240130638a9875647cc4
NOT-FRY <francescod.lauro@gmail.com>
2021-06-11 16:39:56 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/Ackermann32/SocialNotes
    
    Conflicts:
    	SocialNotes/src/Prova.java

--- src/Servlet.java
+++ src/Servlet.java
@@ -4,6 +4,7 @@ public class Servlet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
              System.out.println("Questa è la prima servlet");
+             System.out.println("Forza GIUVE SIUUUUMM");
 	}
 
 }
