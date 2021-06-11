6a9a2f4fc28b0b4a34681aa9165c53760f00d43d
NOT-FRY <francescod.lauro@gmail.com>
2021-06-11 17:45:50 +0200

    ciaociaociao

--- src/Servlet.java
+++ src/Servlet.java
@@ -5,8 +5,12 @@ public class Servlet {
 		// TODO Auto-generated method stub
              System.out.println("Questa Ã¨ la prima servlet");
              System.out.println("Forza GIUVE SIUUUUMM");
-
+             System.out.println("VIVA STALIN U.R.S.S.");
              System.out.println("Questa Ã¨ la seconda servlet");
+             
+             System.out.println("Questa è la servlet con la giusta è accentata");
+             
+             System.out.println("ciaociaociao");
 
 	}
 
